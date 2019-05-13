b1629ce6a9e83eb7842bbabb1383fc510532b4e0
ZAMOUCHE <alizamouche25@gmail.com>
2019-05-14 00:30:53 +0200

    seconde

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
+
+    public static void affiche(String s){
+        System.out.println(" Main " + s);
+    }
     public static void main(String[]agrs){
-        System.out.println(" Main ");
+
+            affiche("ZAMOUCHE Ali");
+
     }
 }
 //https://github.com/users/ZAMOUCHE/projects/2#column-5347687
